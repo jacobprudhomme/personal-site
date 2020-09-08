0f10674badf26a5231082862bc17d2469d097f85
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2020-09-07 23:48:02 -0400

    Rewrote CSS variables in Clay [Part 1]

--- site/css/style.hs
+++ site/css/style.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Clay
 import Data.List.NonEmpty (fromList)
 import Data.Semigroup (sconcat)
 import Prelude hiding (div, span)
 
+import qualified Clay.Flexbox as CF
 import qualified Data.Text.Lazy.IO as T
 
 
@@ -25,9 +28,49 @@ reset = do
       margin (px 0) (px 0) (px 0) (px 0)
       padding (px 0) (px 0) (px 0) (px 0)
 
+-- CSS Variables
+variables :: Css
+variables = do
+  html ? do
+    fontFamily ["Georgia"] [serif]
+    color "#333"
+    backgroundColor "#fffffc"
+    background $
+      linearGradient (straight sideTop)
+        [ ("#eee", 0)
+        , ("#fff", 10)
+        , ("#fff", 90)
+        , ("#eee", 100)
+        ]
+    backgroundAttachment attachFixed
+    height (pct 100)
+    width (pct 100)
+    zIndex (-10)
+
+    ".dark" & do
+      backgroundColor "#333"
+      background $
+        linearGradient (straight sideTop)
+          [ ("#2a2a2a", 0)
+          , ("#333", 10)
+          , ("#333", 90)
+          , ("#2a2a2a", 100)
+          ]
+      backgroundAttachment attachFixed
+      height (pct 100)
+      width (pct 100)
+      color "#ddd"
+
+  body ? do
+    display flex
+    flexFlow column (CF.nowrap)
+    justifyContent center
+
 
 styles :: Css
-styles = reset
+styles = do
+  reset
+  variables
 
 main :: IO ()
 main = T.putStr $ renderWith compact [] styles
